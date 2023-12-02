34bc976e4a80bf6e18abc44db806230ccdcb2ebe
Addiowen <owenoscar18@gmail.com>
2023-12-02 19:08:41 +0300

    updated script file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function togglemenu(){
+function toggleMenu() {
     const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hambager-icon");
+    const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
-}
+  }
